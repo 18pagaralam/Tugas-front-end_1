83c25c896686a8f30516669ed36a67e7e594dd63
18pagaralam <162072594+18pagaralam@users.noreply.github.com>
2024-03-05 08:42:02 +0700

    Add files via upload
    tugas front end 1 fix

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Contoh from './Contoh.jsx'
+import Home from './Pages/home.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/*<Contoh />*/}
+    <Home/>
+  </React.StrictMode>,
+)
